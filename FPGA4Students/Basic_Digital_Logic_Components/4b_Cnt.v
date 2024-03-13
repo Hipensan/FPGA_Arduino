f36f1c820391d3e3d3e666f0d561ce65434704d2
Nuneog <k04005@naver.com>
2024-03-13 16:10:08 +0900

    update basic structure

--- FPGA4Students/Basic_Digital_Logic_Components/4b_Cnt.v
+++ FPGA4Students/Basic_Digital_Logic_Components/4b_Cnt.v
@@ -1,4 +1,14 @@
 module Cnt_4b(
-
+	input i_Clk,
+	input i_Rst,
+	output reg [3:0] o_Cnt
 	);
 
+always@(posedge i_Clk, negedge i_Rst)
+	if(!i_Rst) begin
+		o_Cnt <= 0;
+	end else begin
+		o_Cnt <= o_Cnt + 1;
+	end
+
+endmodule
--- FPGA4Students/Basic_Digital_Logic_Components/LFSR.v
+++ FPGA4Students/Basic_Digital_Logic_Components/LFSR.v
@@ -1,7 +1,50 @@
-/*
-LFSR
-	L : Linear
-	F : Feedback
-	S : Shift
-	R : Register
-*/
+/*######################
+	LFSR
+		L : Linear
+		F : Feedback
+		S : Shift
+		R : Register
+#######################*/
+module LFSR(
+	input i_Clk,
+	input i_Rst,
+	input i_en,
+	input [4:0] S_initial,
+	output [4:0] S_out
+	);
+
+reg [4:0] 	_reg		, n_reg;
+reg 		c_state		, n_state;
+// XOR with reg[4], reg[2], reg[1]
+
+
+always@(posedge i_Clk, negedge i_Rst)
+	if(!i_Rst) begin
+		c_reg 	<= S_initial;
+		c_state <= 0;
+	end else begin
+		c_reg 	<= n_reg;
+		c_state <= n_state;
+	end
+
+
+always@* begin
+	n_state <= c_state;
+	n_reg 	<= c_reg;
+
+	case(c_state)
+		1'b0 : 			// IDLE
+			if(i_en) n_state <= 1'b1;
+		
+
+		1'b1 : begin 	// WORK
+			if(i_en)  	//LFSR work
+				n_reg = {reg[3], reg[2], reg[1], reg[0], (reg[4] ^ reg[2] ^ reg[1])};
+			
+			else 
+				n_state <= 1'b0;
+		end
+	endcase 
+
+	assign S_out = c_reg;
+end
